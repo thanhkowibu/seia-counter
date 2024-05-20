85728cc20ae9b7f0b922b66b881e964d5f2a40e2
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2024-05-20 22:39:26 +0700

    fix days count issues

--- src/components/common/Time/calculateGapTime.ts
+++ src/components/common/Time/calculateGapTime.ts
@@ -1,15 +1,15 @@
-import { intervalToDuration } from "date-fns";
+import { differenceInDays, intervalToDuration } from "date-fns";
 const calculateGapTime = () => {
   const now = new Date();
   const startDate = new Date(2024, 3, 20, 0, 0, 0);
-  const durationGap = intervalToDuration({ start: startDate, end: now });
+  const daysGap = differenceInDays(now, startDate);
   const tomorrow = new Date(now);
   tomorrow.setDate(tomorrow.getDate() + 1);
   tomorrow.setHours(0, 0, 0, 0); // set time to next day 00:00:00
   const durationTomorrow = intervalToDuration({ start: now, end: tomorrow });
 
   return {
-    days: durationGap.days ?? 0,
+    days: daysGap,
     hours: (durationTomorrow.hours ?? 0).toString().padStart(2, "0"),
     minutes: (durationTomorrow.minutes ?? 0).toString().padStart(2, "0"),
     seconds: (durationTomorrow.seconds ?? 0).toString().padStart(2, "0"),
